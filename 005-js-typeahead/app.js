c663cbefcb96072fa9b9ef1856e7b0fbcafa7f0a
ukasz09 <246703@student.pwr.edu.pl>
2021-08-24 16:10:28 +0200

    Highlight matched words

--- 005-js-typeahead/app.js
+++ 005-js-typeahead/app.js
@@ -23,7 +23,7 @@ const addOnInputHandler = () => {
 const personIsMatch = (person, searchText) => {
   for (const field of fieldsUsedInSearch) {
     const fieldValue = person[field];
-    if (fieldValue && fieldValue.toLowerCase().includes(searchText)) {
+    if (fieldValue && fieldValue.toLowerCase().trim().includes(searchText)) {
       return true;
     }
   }
@@ -40,25 +40,67 @@ const search = (text) => {
       }
     });
   }
-  updateTypeahead(searchResult);
+  updateTypeahead(searchResult, parsedText);
 };
 
-const updateTypeahead = (matchedPeoples) => {
+const updateTypeahead = (matchedPeoples, searchedText) => {
   // Remove not actual rows
-  const oldRows = typeaheadArea.querySelectorAll("div");
+  const oldRows = typeaheadArea.querySelectorAll(".typeahead-row");
   oldRows.forEach((row) => typeaheadArea.removeChild(row));
 
   // Add actual rows
   matchedPeoples.forEach((person) => {
-    const rowDiv = getTypeaheadRow(person);
+    const rowDiv = getTypeaheadRow(person, searchedText);
     typeaheadArea.appendChild(rowDiv);
   });
 };
 
-const getTypeaheadRow = (person) => {
+const getTextDivWithMatchedPartBold = (text, boldText) => {
+  const startIndex = text.toLowerCase().indexOf(boldText);
+
+  //   Create div with normal text
+  if (startIndex === -1) {
+    const rowDiv = document.createElement("div");
+    const textSpan = document.createElement("span");
+    textSpan.textContent = text;
+    rowDiv.appendChild(textSpan);
+    return rowDiv;
+  }
+
+  // Create div with bolded text
+  const fstNormalTextPart = text.substring(0, startIndex);
+  const boldedTextEndIndex = startIndex + boldText.length;
+  const boldedTextPart = text.substring(startIndex, boldedTextEndIndex);
+  const lstNormalTextPart = text.substring(boldedTextEndIndex, text.length);
+
   const rowDiv = document.createElement("div");
-  rowDiv.classList.value = ["btn btn-light text-left w-100 rounded-0 border slide-in-top"];
-  rowDiv.textContent = `${person.first_name} ${person.last_name}`;
+  if (fstNormalTextPart) {
+    const textSpan = document.createElement("span");
+    textSpan.textContent = fstNormalTextPart;
+    rowDiv.appendChild(textSpan);
+  }
+  if (boldedTextPart) {
+    const textSpan = document.createElement("span");
+    textSpan.classList = ["font-weight-bold text-primary"];
+    textSpan.textContent = boldedTextPart;
+    rowDiv.appendChild(textSpan);
+  }
+  if (lstNormalTextPart) {
+    const textSpan = document.createElement("span");
+    textSpan.textContent = lstNormalTextPart;
+    rowDiv.appendChild(textSpan);
+  }
+  return rowDiv;
+};
+
+const getTypeaheadRow = (person, searchedText) => {
+  const rowDiv = document.createElement("div");
+  rowDiv.classList.value = ["d-flex flex-row btn btn-light text-left w-100 rounded-0 border slide-in-top typeahead-row"];
+  const fstNameDiv = getTextDivWithMatchedPartBold(person.first_name.trim(), searchedText);
+  fstNameDiv.classList.add("mr-1");
+  rowDiv.appendChild(fstNameDiv);
+  const lstNameDiv = getTextDivWithMatchedPartBold(person.last_name.trim(), searchedText);
+  rowDiv.appendChild(lstNameDiv);
   return rowDiv;
 };
 
