6840cbe41963545bb30092fb9a38f37c4eb38bdd
unknown <gajerski.lukasz@gmail.com>
2022-03-06 10:03:48 +0100

    Add method 2 for title-case cata

--- 007-title-case/index.js
+++ 007-title-case/index.js
@@ -8,16 +8,29 @@ const addTransformButtonClickListener = () => {
 const displayTitleCased = () => {
     const inputElement = document.querySelector('input#value');
     const rawValue = inputElement.value;
-    const titleCased1 = makeTitleCaseMethod1(rawValue);
 
+    const titleCased1 = makeTitleCaseMethod1(rawValue);
     const resultElement1 = document.querySelector('#method-1-result');
     resultElement1.textContent = titleCased1;
+
+    const titleCased2 = makeTitleCaseMethod2(rawValue);
+    const resultElement2 = document.querySelector('#method-2-result');
+    resultElement2.textContent = titleCased2;
 }
 
 const makeTitleCaseMethod1 = (value, wordsSeparator = ' ') => {
     return value.split(wordsSeparator).map(
         w => w[0] ? `${w[0].toUpperCase()}${w.slice(1, w.length)}` : w
     ).join(wordsSeparator);
+}
+
+const makeTitleCaseMethod2 = (value, wordsSeparator = ' ') => {
+    const transformedWords = [];
+    for (const w of value.split(wordsSeparator)) {
+        const transformed = w[0] ? w[0].toUpperCase() + w.slice(1, w.length) : w
+        transformedWords.push(transformed);
+    }
+    return transformedWords.join(wordsSeparator);
 }
 
 addTransformButtonClickListener();
